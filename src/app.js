d6e4726258d7131523419d92fedf0f61904be44f
mimietina <michelletinashe03@gmail.com>
2024-06-20 17:53:14 +0200

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector(".weather-app-temperature");
+  let temperature = response.data.temperature.current
+  let cityElement = document.querySelector("#city");
+    
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature)
+
+}
+
+function searchCity(city) {
+let apiKey = "4boe913d4579b6981f129f90tda79ab0";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.textContent = searchInput.value;
+  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Ottawa");
